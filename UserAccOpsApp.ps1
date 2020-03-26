5f205b04290d3dc78d75d50acaa7915478b9419a
Josef Ereq <52008857+JosefEreq@users.noreply.github.com>
2020-03-26 17:06:33 +0100

    Update UserAccOpsApp.ps1

--- UserAccOpsApp.ps1
+++ UserAccOpsApp.ps1
@@ -92,7 +92,6 @@ import-module activedirectory
     $DateRange = (import-csv (join-path $GlobalData "AppDateRange.csv") -Delimiter ";")
 
     ## - Set the path to the output-files in variables. These files will be picked up and executed by the back-end script.
-    	USE THE ONE UNIVERSAL BACK-END SCRIPT FROM DOWNLOAD - OR SPLIT IT UPP IF YOU WISH
         
         ### - The output-file for the password-reset back-end script.
         $InputFilePWRes = (join-path $ScriptData "INPUT_UserAccPWReset.txt")
